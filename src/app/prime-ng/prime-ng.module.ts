bf06db065d20d3d260a05ef4cdfe4783ce45ace4
Karen Plata <karen.platahdz22@outlook.es>
2024-02-26 23:47:35 -0600

    menu bar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 // Módulos Prime Ng
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+import {InputTextModule} from 'primeng/inputtext';
 
 
 @NgModule({
   exports: [
     MenuModule,
-    ButtonModule
+    ButtonModule,
+    MenubarModule,
+    InputTextModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -11,9 +11,34 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.menuItems = [
-      {label: 'New', icon: 'pi pi-fw pi-plus'},
-      {label: 'Open', icon: 'pi pi-fw pi-download'},
-      {label: 'Undo', icon: 'pi pi-fw pi-refresh'}
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe'
+          }
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
+      }
     ]
   }
 

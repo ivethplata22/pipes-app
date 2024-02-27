3ce6a29c1beaa52163e5ff12f2c7206dcf91fa94
Karen Plata <karen.platahdz22@outlook.es>
2024-02-26 23:56:47 -0600

    rutas

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+
+}
